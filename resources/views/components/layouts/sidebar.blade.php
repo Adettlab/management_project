4db5ce505c2e32963288af10049e545d42d31642
Saitampool <dimashasim1106@gmail.com>
2025-05-21 13:20:52 +0700

    rebuild sidebar

--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="sidebar-container fixed h-screen transition-all duration-300"
     :class="{ 'w-[8.5%]': !sidebarExpanded, 'w-[14%]': sidebarExpanded }" x-init="document.querySelector('.sidebar-container').classList.add(sidebarExpanded ? 'sidebar-init-expanded' : 'sidebar-init-collapsed')">
     <!-- Toggle Button -->
-    <button @click="toggleSidebar()"
+    {{-- <button @click="toggleSidebar()"
         class="absolute -right-3 bg-secondary-white text-[#616161] border rounded-full p-1 z-[55] hover:bg-[#6FAEC9] hover:text-white">
         <svg x-show="!sidebarExpanded" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
             fill="currentColor">
@@ -15,10 +15,27 @@
                 d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
                 clip-rule="evenodd" />
         </svg>
+    </button> --}}
+    <button @click="toggleSidebar()"
+        class="absolute h-6 w-6 bg-white border border-gray-200 text-[#616161] rounded-full z-[999] hover:bg-[#6FAEC9] hover:text-white"
+        style="transform: translateX(0); right: 0;">
+        <!-- Always show one icon or the other -->
+        <svg x-cloak x-show="!sidebarExpanded" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+            fill="currentColor">
+            <path fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd" />
+        </svg>
+        <svg x-cloak x-show="sidebarExpanded" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+            fill="currentColor">
+            <path fill-rule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clip-rule="evenodd" />
+        </svg>
     </button>
     <nav class="pt-8 items-center bg-white flex flex-col h-full relative border-r z-50">
-        <div class="flex flex-col space-y-7 w-full px-3"
-            :class="{ '': sidebarExpanded, 'items-center': !sidebarExpanded }">
+        <div class="flex flex-col w-full"
+            :class="{ 'justify-start px-2': sidebarExpanded, 'items-center': !sidebarExpanded }">
             {{-- dashboard --}}
             <x-sidebar.dashboard :active="$active" :expanded="true" />
 
--- resources/views/components/sidebar/tasks.blade.php
+++ resources/views/components/sidebar/tasks.blade.php
@@ -1,31 +1,28 @@
 @props(['active', 'expanded' => false])
-<div class="group flex items-center z-50 cursor-pointer {{ $active === 'tasks' ? 'bg-sky-blue' : 'bg-white' }}"
+<div class="group w-full mb-3 flex items-center z-50 cursor-pointer"
     :class="{
-        'justify-center rounded-full': !
-            sidebarExpanded,
-        'hover:bg-[#6FAEC9] hover:text-white rounded-full': sidebarExpanded
+        'justify-center rounded-full': !sidebarExpanded,
+        'hover:bg-blue-400 hover:text-white rounded-full {{ $active === 'tasks' ? 'bg-blue-300' : '' }}': sidebarExpanded
     }"
     onclick="window.location.href='{{ route('tasks.index') }}'">
-    <a class="bg-secondary-white p-2 flex items-center justify-center rounded-full cursor-pointer group-hover:bg-[#6FAEC9] {{ $active === 'tasks' ? 'bg-sky-blue' : 'bg-white' }}"
-        :class="{ 'group-hover:text-white rounded-full': sidebarExpanded, 'rounded-full': !sidebarExpanded }"
-        href="{{ route('tasks.index') }}">
-        <svg class="size-6" viewBox="0 0 35 35" fill="{{ $active === 'tasks' ? '#FFFFFF' : '#616161' }}"
-            xmlns="http://www.w3.org/2000/svg">
-            <path class="{{ $active === 'tasks' ? 'fill-[#FFFFFF]' : 'fill-[#616161]' }} group-hover:fill-[#FFFFFF]"
+    <a href="{{ route('tasks.index') }}"
+        class="p-2 flex items-center justify-center rounded-full cursor-pointer group-hover:bg-blue-400"
+        :class="{
+            'group-hover:text-white rounded-full {{ $active === 'tasks' ? 'bg-blue-300' : 'bg-white' }}': sidebarExpanded,
+            'rounded-full {{ $active === 'tasks' ? 'bg-blue-300' : 'bg-gray-200' }}': !sidebarExpanded
+        }">
+        <svg class="size-6" viewBox="0 0 35 35" xmlns="http://www.w3.org/2000/svg">
+            <path class="{{ $active === 'tasks' ? 'fill-white' : 'fill-gray-600' }} group-hover:fill-white"
                 d="M20.4154 2.9165H8.7487C7.14453 2.9165 5.84661 4.229 5.84661 5.83317L5.83203 29.1665C5.83203 30.7707 7.12995 32.0832 8.73411 32.0832H26.2487C27.8529 32.0832 29.1654 30.7707 29.1654 29.1665V11.6665L20.4154 2.9165ZM15.9529 26.2498L10.7904 21.0873L12.8466 19.0311L15.9383 22.1228L22.1216 15.9394L24.1779 17.9957L15.9529 26.2498ZM18.957 13.1248V5.104L26.9779 13.1248H18.957Z" />
         </svg>
     </a>
 
     <!-- Label - Shows when sidebar is expanded or on hover when collapsed -->
-    <a href="{{ route('tasks.index') }}" x-show="sidebarExpanded"
-        class="ml-1 pr-2 text-gray-700 text-sm font-medium {{ $active === 'tasks' ? 'text-white' : '' }}"
-        :class="{ 'group-hover:text-white': sidebarExpanded }">
+    <a href="{{ route('tasks.index') }}" x-show="sidebarExpanded" class="ml-1 pr-2 text-sm font-medium"
+        :class="{
+            'group-hover:text-white {{ $active === 'tasks' ? 'text-white' : '' }}': sidebarExpanded,
+            '': !sidebarExpanded
+        }">
         Task
     </a>
-
-    <!-- Tooltip - Shows only on hover when sidebar is collapsed -->
-    <div style="z-index: 999;" x-show="!sidebarExpanded"
-        class="hidden group-hover:block text-sm text-white text-center w-32 py-[2px] rounded-md absolute bg-sky-blue left-24">
-        Task
-    </div>
 </div>
